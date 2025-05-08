1bd85d96ca2f723c41a560989c75c70cb8e16e01
cgrace0044 <cgrace0044@gmail.com>
2025-05-08 12:59:54 +0200

    protect secret key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_dYrmbUCO8Ln7@ep-jolly-heart-a28o1qh5.eu-central-1.aws.neon.tech/snare_wrath_stamp_616879")
+os.environ.setdefault("SECRET_KEY", "<1Dublin.>")
