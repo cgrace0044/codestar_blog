bae622ebcbc2ead8e800e223b085a563a3cee15a
cgrace0044 <cgrace0044@gmail.com>
2025-05-15 14:10:53 +0200

    add comment feature to code

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
+
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
@@ -25,9 +26,15 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    request,
+    "blog/post_detail.html",
+    {
+        "post": post,
+        "comments": comments,
+        "comment_count": comment_count,
+    },
+)
